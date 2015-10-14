9738032bad82aa67334adb6f3f436407c190d2e6
Simeon Ackermann <s.ackermann@mail.de>
2015-10-14 17:00:24 +0200

    Update logo. Set navigation defaults. Disable jobcenter.

--- config/ontowiki/config-run.sh
+++ config/ontowiki/config-run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# add root index.html
-cp /config/index.html /var/www/
-
 # copy new header logo
-cp /config/logo-ontowiki.png /var/www/ontowiki/extensions/themes/amsl/images/
+cp /config/logo-ontowiki.png /var/www/extensions/themes/amsl/images/
+
+# disable jobcenter extension
+cp /config/jobcenter.ini /var/www/extension/
+
+# set navigation extension
+cp /config/navigation.ini /var/www/extension/
