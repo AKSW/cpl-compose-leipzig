9305541c9ef4c92e1bef790be6b0df9d87d43e90
Simeon Ackermann <s.ackermann@mail.de>
2015-10-23 12:39:08 +0200

    Fix typo extensions path

--- config/ontowiki/config-run.sh
+++ config/ontowiki/config-run.sh
@@ -4,7 +4,7 @@
 cp /config/logo-ontowiki.png /var/www/extensions/themes/amsl/images/
 
 # disable jobcenter extension
-cp /config/jobcenter.ini /var/www/extension/
+cp /config/jobcenter.ini /var/www/extensions/
 
 # set navigation extension
-cp /config/navigation.ini /var/www/extension/
+cp /config/navigation.ini /var/www/extensions/
